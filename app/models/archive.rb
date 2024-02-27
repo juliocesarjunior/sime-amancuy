d8caabd04a2033f474d86b954433d21cd41887a7
juliocesarjunior <julio.cesar3151@hotmail.com.br>
2024-02-27 17:38:06 -0300

    model

--- app/models/archive.rb
+++ app/models/archive.rb
@@ -1,3 +1,12 @@
 class Archive < ApplicationRecord
+	audited
+
 	belongs_to :fileable, polymorphic: true
+
+	validates_uniqueness_of :fileable_id, scope: :fileable_type
+
+
+	def destroy
+		self.update_attribute(:status, 2)
+	end
 end
--- app/models/phalange.rb
+++ app/models/phalange.rb
@@ -1,2 +1,10 @@
 class Phalange < ApplicationRecord
+	audited
+
+	enum status: { active: 0, inactive: 1, deleted: 2 }, _default: :active
+
+	
+	def destroy
+		self.update_attribute(:status, 2)
+	end
 end
